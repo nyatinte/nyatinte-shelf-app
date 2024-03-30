d2add07d4ea838db46898e86ed42a4f5eb2a6dde
nyatinte <takumi.062757@gmail.com>
2024-03-30 21:25:33 +0900

    Apply biome check

--- src/lib/scraper.ts
+++ src/lib/scraper.ts
@@ -1,30 +1,30 @@
 export const scraper = async (url: string) => {
-  const res = await fetch(url);
-  const html = await res.text();
+  const res = await fetch(url)
+  const html = await res.text()
 
-  const titleMatch = html.match(/<title>(.*?)<\/title>/);
-  const title = titleMatch ? titleMatch[1] : null;
+  const titleMatch = html.match(/<title>(.*?)<\/title>/)
+  const title = titleMatch ? titleMatch[1] : null
 
   const descriptionMatch =
     html.match(/<meta name="description" content="(.*?)"/) ||
-    html.match(/<meta property="og:description" content="(.*?)"/);
-  const description = descriptionMatch ? descriptionMatch[1] : null;
-  let imageMatch = html.match(/<meta property="og:image" content="(.*?)"/);
-  let image = imageMatch ? imageMatch[1] : null;
+    html.match(/<meta property="og:description" content="(.*?)"/)
+  const description = descriptionMatch ? descriptionMatch[1] : null
+  const imageMatch = html.match(/<meta property="og:image" content="(.*?)"/)
+  let image = imageMatch ? imageMatch[1] : null
 
   if (image?.startsWith('/')) {
-    const urlObj = new URL(url);
-    image = `${urlObj.origin}${image}`;
+    const urlObj = new URL(url)
+    image = `${urlObj.origin}${image}`
   }
 
-  let faviconMatch =
+  const faviconMatch =
     html.match(/<link rel="icon" href="(.*?)"/) ||
-    html.match(/<link rel="shortcut icon" href="(.*?)"/);
-  let favicon = faviconMatch ? faviconMatch[1] : null;
+    html.match(/<link rel="shortcut icon" href="(.*?)"/)
+  let favicon = faviconMatch ? faviconMatch[1] : null
 
   if (favicon?.startsWith('/')) {
-    const urlObj = new URL(url);
-    favicon = `${urlObj.origin}${favicon}`;
+    const urlObj = new URL(url)
+    favicon = `${urlObj.origin}${favicon}`
   }
 
   return {
@@ -32,5 +32,5 @@ export const scraper = async (url: string) => {
     description,
     image,
     favicon,
-  };
-};
+  }
+}
